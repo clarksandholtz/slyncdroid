ddaa8b38f964c1c4b821b0e9c52c8efa3a51494c
undermark5 <nsshurtz@gmail.com>
2018-02-21 10:54:47 -0700

    reorganized some resources. added the fonts and colors into the xml.

--- app/src/main/java/com/get_slyncy/slyncy/View/SettingsActivity.java
+++ app/src/main/java/com/get_slyncy/slyncy/View/SettingsActivity.java
@@ -3,7 +3,6 @@ package com.get_slyncy.slyncy.View;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -18,49 +17,54 @@ import com.get_slyncy.slyncy.R;
 
 public class SettingsActivity extends Activity
 {
+    private TextView accountName;
+    private TextView accountEmail;
+    private TextView accountPhone;
+    private TextView syncEntry;
+    private TextView groupMessageEntry;
+    private TextView notifEntry;
+    private TextView logoutEntry;
+
+    private ImageView accountProfile;
+    private ImageView syncIcon;
+    private ImageView groupIcon;
+    private ImageView notifIcon;
+    private ImageView logoutIcon;
+    private ImageView notifArrow;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/regular.ttf");
-        TextView accountName = findViewById(R.id.account_name);
-        TextView accountEmail = findViewById(R.id.account_email);
-        TextView accountPhone = findViewById(R.id.account_phone);
-        accountName.setTypeface(typeface);
-        accountEmail.setTypeface(typeface);
-        accountPhone.setTypeface(typeface);
-        accountName.setTextColor(getColor(R.color.colorFontLight));
-        accountEmail.setTextColor(getColor(R.color.colorFontLight));
-        accountPhone.setTextColor(getColor(R.color.colorFontLight));
 
-        TextView syncEntry = findViewById(R.id.sync_entry_title);
-        TextView groupMessageEntry = findViewById(R.id.group_message_entry);
-        TextView notifEntry = findViewById(R.id.notification_entry);
-        TextView logoutEntry = findViewById(R.id.logout_entry);
-        syncEntry.setTypeface(typeface);
-        groupMessageEntry.setTypeface(typeface);
-        notifEntry.setTypeface(typeface);
-        logoutEntry.setTypeface(typeface);
-        syncEntry.setTextColor(getColor(R.color.colorFontDark));
-        groupMessageEntry.setTextColor(getColor(R.color.colorFontDark));
-        notifEntry.setTextColor(getColor(R.color.colorFontDark));
-        logoutEntry.setTextColor(getColor(R.color.colorFontDark));
+        accountName = findViewById(R.id.account_name);
+        accountEmail = findViewById(R.id.account_email);
+        accountPhone = findViewById(R.id.account_phone);
 
 
+        syncEntry = findViewById(R.id.sync_entry_title);
+        groupMessageEntry = findViewById(R.id.group_message_entry);
+        notifEntry = findViewById(R.id.notification_entry);
+        logoutEntry = findViewById(R.id.logout_entry);
 
-        ImageView accountProfile = findViewById(R.id.account_profile);
-        ImageView syncIcon = findViewById(R.id.sync_icon);
-        ImageView groupIcon = findViewById(R.id.group_message_icon);
-        ImageView notifIcon = findViewById(R.id.notification_icon);
-        ImageView logoutIcon = findViewById(R.id.logout_icon);
-        ImageView notifArrow = findViewById(R.id.notification_arrow);
+        accountProfile = findViewById(R.id.account_profile);
+        syncIcon = findViewById(R.id.sync_icon);
+        groupIcon = findViewById(R.id.group_message_icon);
+        notifIcon = findViewById(R.id.notification_icon);
+        logoutIcon = findViewById(R.id.logout_icon);
+        notifArrow = findViewById(R.id.notification_arrow);
         accountProfile.setImageDrawable(getDrawable(R.drawable.ic_account_circle_black_24dp));
         syncIcon.setImageDrawable(getDrawable(R.drawable.ic_sync_black_24dp));
         groupIcon.setImageDrawable(getDrawable(R.drawable.ic_group_black_24dp));
         notifIcon.setImageDrawable(getDrawable(R.drawable.ic_notifications_black_24dp));
         logoutIcon.setImageDrawable(getDrawable(R.drawable.ic_logout_black_24dp));
         notifArrow.setImageDrawable(getDrawable(R.drawable.ic_navigate_next_black_24dp));
+        accountProfile.setColorFilter(getColor(R.color.white));
+        syncIcon.setColorFilter(getColor(R.color.colorFontDark));
+        groupIcon.setColorFilter(getColor(R.color.colorFontDark));
+        logoutIcon.setColorFilter(getColor(R.color.colorFontDark));
+        notifArrow.setColorFilter(getColor(R.color.colorFontDark));
+        notifIcon.setColorFilter(getColor(R.color.colorFontDark));
     }
 
     public void launchNotifFilter(View view)
